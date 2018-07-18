0ab3dd49eb18780107399e52b5070a6015586fc7
Daksha Patel <dakshapatel100@gmail.com>
2018-07-18 17:41:54 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,7 +6,8 @@ class App < Sinatra::Base
     erb :index
   end
 
-  post '/' do 
+  post '/' do
+    text_from_user = params[:user_text]
     @analyzed_text = TextAnalyzer.new(params[:user_text])
 
     erb :results
